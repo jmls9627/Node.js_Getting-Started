9daeeff13fc0ae3c8c859935f12e10e74ba97020
unknown <jmls9627@gmail.com>
2020-11-02 10:43:56 -0500

    4.1_Defining and Using Node-Modules

---
+++ 4.Module/1-define-use/3-wrapper.js
@@ -0,0 +1,17 @@
+// function (export, module, require, __filename, __dirname){
+
+// let g = 1;
+
+//console.log(arguments);
+
+exports.a=42;
+module.exports.b=37;
+
+//export = () => {}; // not ok
+
+module.exports=()=>{};
+
+//return module.exports;
+//} (Module.exports,)
+
+
